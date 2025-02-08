d7e2582b96d70b55542d31c1790c407339578f95
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-02-08 13:30:55 +0700

    task2_number

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 //console.log("Hello, Typescript with Node.js");
 let x: number | string = 10;
-x = 'hello';
+x = 10;
 if (typeof x === 'string') {
     console.log('x is string');
 }
-else if (typeof x === 'string') {
+else if (typeof x === 'number') {
     console.log('x is number');
 }
 else {
