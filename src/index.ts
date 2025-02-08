241d92ba28cdec2ada4c23d7c6168eac868682b8
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-02-08 13:38:20 +0700

    task3_number

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 //console.log("Hello, Typescript with Node.js");
-let x: number | string = 10;
-x = 10;
-if (typeof x === 'string') {
-    console.log('x is string');
+
+//task2
+//let x: number | string = 10;
+//x = 10;
+//if (typeof x === 'string') {
+//    console.log('x is string');
+//}
+//else if (typeof x === 'number') {
+//    console.log('x is number');
+//}
+//else {
+//    console.log('x is neither string nor number');
+//}
+
+//task3
+const add = (a:number, b:number) => {
+    return a+b;
 }
-else if (typeof x === 'number') {
-    console.log('x is number');
-}
-else {
-    console.log('x is neither string nor number');
-}
+const result = add(1,2) + 0;
+console.log(result, 'type of result: ', typeof result);
